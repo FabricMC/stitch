f57fe7ce862032ca9536cea96488c7255b9c81f4
Fudge <natan.lifsiz@gmail.com>
2019-11-04 16:35:28 +0200

    fix license headers and tests (#18)

--- src/test/java/net/fabricmc/stitch/tinyv2/Snapshot37a.java
+++ src/test/java/net/fabricmc/stitch/tinyv2/Snapshot37a.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.stitch.tinyv2;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class Snapshot37a {
@@ -38,6 +56,7 @@ public class Snapshot37a {
 
 	/** You need the 19w37a-merged.jar file under local/ */
 	@Test
+	@Disabled
 	public void testFieldNameProposal() throws Exception {
 		Commands.proposeFieldNames("local/19w37a-merged.jar",
 						DIR + "merged.tinyv2", DIR + "merged-proposed.tinyv2");
